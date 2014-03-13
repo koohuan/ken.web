27e3286b2077f43396b1c378c8a5540f8473542b
nonetaichi <68103403@qq.com>
2014-03-13 18:09:27 +0800

    functions.php

---
+++ src/functions.php
@@ -0,0 +1,57 @@
+<?php
+/**
+  判断是否Ajax请求
+*/
+function is_ajax(){
+    if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+/**
+	刷新当前页面
+*/
+function refresh(){
+	header("Refresh:0");
+}
+/**
+	创建URL
+*/
+function url($url , $par = []){
+	return F::get('route')->url($url,$par);
+}
+/**
+	取得当前HOST http://yuetaichi.com 
+*/
+function host(){
+	return F::get('route')->host;
+}
+/**
+	URL是对public 的而言
+	返回URL 如 / 或 web/public/
+*/
+function base_url(){ 
+	return F::get('route')->base_url; 
+} 
+/**
+  翻译
+*/
+function __($key,$alias='app'){ 
+	return F::get('lang')->get($key,$alias); 
+}  
+function redirect($url){
+	header("location:$url"); 
+	exit;
+} 
+function dump($str){
+	print_r('<pre>');
+	print_r($str);
+	print_r('</pre>');
+}   
+function ip() {
+    return $_SERVER['REMOTE_ADDR'];
+}
