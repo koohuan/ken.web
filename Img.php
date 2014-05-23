5dd739f60d6d4ac6dbea0a33c6a61212c43be61b
nonetaichi <68103403@qq.com>
2014-05-23 16:02:01 +0800

    fixed Auth

--- Img.php
+++ Img.php
@@ -34,34 +34,34 @@ class Img
  	* 本地的图片
  	* 如果存在返回图片的URL
  	*/
-	function local_one($str){
-		return $this->local($str , false);
+	static function local_one($str){
+		return static::local($str , false);
 	}
 	/**
  	* 本地的所有图片
  	* 返回数组，元素为URL
  	*/
-	function local_all($str){
-		return $this->local($str , true);
+	static function local_all($str){
+		return static::local($str , true);
 	}
 	/**
 	* 不区别本地或线上图片
 	* 返回第一个图片的URL
 	*/
-	function get_one($str){
-		return $this->get($str , false);
+	static function get_one($str){
+		return static::get($str , false);
 	}
 	/**
 	* 不区别本地或线上图片
 	* 数组格式 返回所有图片的URL
 	*/
-	function get_all($str){
-		return $this->get($str , true);
+	static function get_all($str){
+		return static::get($str , true);
 	}
 	/**
 	* 移除内容中的图片元素
 	*/
-	function remove($content){  
+	static function remove($content){  
 		$preg = '/<\s*img\s+[^>]*?src\s*=\s*(\'|\")(.*?)\\1[^>]*?\/?\s*>/i';
 		$out = preg_replace($preg,"",$content);
 		return $out;
@@ -69,12 +69,12 @@ class Img
 	/**
 	* 图片的宽高
 	*/
-	function wh($img){
+	static function wh($img){
 		$a = getimagesize(root_path().$img);
 		return array('w'=>$a[0],'h'=>$a[1]);
 	}
  
-	protected function get($content,$all=true){ 
+	static function get($content,$all=true){ 
 		$preg = '/<\s*img\s+[^>]*?src\s*=\s*(\'|\")(.*?)\\1[^>]*?\/?\s*>/i'; 
 		preg_match_all($preg,$content,$out);
 		$img = $out[2];  
@@ -85,8 +85,8 @@ class Img
 		}
 		return $out[0];
 	} 
-	function local($content,$all=false){  
-		$img = $this->get($content, true);
+	static function local($content,$all=false){  
+		$img = static::get($content, true);
 		if($img) { 
 			$num = count($img); 
 			for($j=0;$j<$num;$j++){ 
